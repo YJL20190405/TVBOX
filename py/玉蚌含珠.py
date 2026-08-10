b72498d1781aa48ca9f7c93499cf52dc0307858e
杨少侠 <11900663+youngxiayang@user.noreply.gitee.com>
2026-08-10 17:19:10 +0000

    update py/玉蚌含珠.py.
    Signed-off-by: 杨少侠 <11900663+youngxiayang@user.noreply.gitee.com>

--- py/玉蚌含珠.py
+++ py/玉蚌含珠.py
@@ -18,7 +18,7 @@ from base.spider import Spider
 
 class Spider(Spider):
     session = requests.Session()
-    host = 'https://o1p2q3r4.ybhz44.cc/yb/'
+    host = 'https://o1p2q3r4.ybhz44.cc'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
