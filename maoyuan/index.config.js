77f95a9223e57f6848c2317dde2bb43c76bc3c03
YJL20190405 <526898155@qq.com>
2026-09-05 00:29:44 +0800

    Update index.config.js

--- maoyuan/index.config.js
+++ maoyuan/index.config.js
@@ -64,7 +64,8 @@ var index_config_default = {
     cookie: ""
   },
   bili: {
-    cookie: ""
+    cookie: "",
+    configUrl: "https://raw.githubusercontent.com/YJL20190405/TVBOX/refs/heads/main/Bilibili/BLHJ.json"
   },
   muou: {
     url: ""
