28e1d8cb6b50547b9d683967af8bca1a8c557f4e
YJL20190405 <526898155@qq.com>
2026-09-05 03:06:30 +0800

    Update index.config.js

--- maoyuan/index.config.js
+++ maoyuan/index.config.js
@@ -67,6 +67,9 @@ var index_config_default = {
     cookie: "",
     configUrl: "https://raw.githubusercontent.com/YJL20190405/TVBOX/refs/heads/main/Bilibili/BLHJ.json"
   },
+  acfanh5: {
+    scriptUrl: "https://raw.githubusercontent.com/YJL20190405/TVBOX/refs/heads/main/maoyuan/acfanh5.js"
+  },
   muou: {
     url: ""
   },
